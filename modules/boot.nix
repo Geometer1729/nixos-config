d2738aff082aad1b22b5d4294cb0e73663afd769
Geometer1729 <16kuhnb@gmail.com>
2023-05-17 12:41:38 -0400

    update

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
   boot= {
-    cleanTmpDir = true;
+    tmp.cleanOnBoot = true;
     loader = {
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot/efi";
-      #systemd-boot.enable = true;
       systemd-boot = {
         enable = true;
         configurationLimit = 20;
