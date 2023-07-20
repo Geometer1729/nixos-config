96686d5259a5f9338b985fe8f87705eeaf8b0e0a
Geometer1729 <16kuhnb@gmail.com>
2023-07-20 18:07:12 -0400

    tweak vimwiki settings

--- home/nvim/lua/vw.lua
+++ home/nvim/lua/vw.lua
@@ -1,4 +1,5 @@
 local g = vim.g
+local o = vim.o
 
 g.vimwiki_list = {
   { path = '~/Documents/vw/',
@@ -7,6 +8,11 @@ g.vimwiki_list = {
     links_space_char= '_',
     auto_diary_index = 1
   }}
+
+-- Don't conceal markdown in any mode
+o.concealcursor=""
+-- Tells task wiki not to break this
+g.taskwiki_disable_concealcursor=1
 
 vim.cmd.highlight({"link","VimWikiHeader1","Identifier"})
 vim.cmd.highlight({"link","VimWikiHeader2","Identifier"})
@@ -21,11 +27,10 @@ g.taskwiki_data_location="~/.local/share/task"
 g.taskwiki_dont_fold="yes"
 g.vimwiki_folding=''
 g.foldenable=false
+g.foldmethod="syntax"
 
 vim.api.nvim_create_autocmd({'BufNewFile'},
   { pattern = '*Documents/vw/diary/*',
     command = [[silent :0r !cat ~/Documents/vw/templates/diary.md | sed "s/DATE/$(date '+\%m\/\%d\/\%y\ \%A')/g"]]
   }
   )
-
-
