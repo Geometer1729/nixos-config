259ac1aca7864f6e0ded47200fb21a1196ee387a
Geometer1729 <16kuhnb@gmail.com>
2023-11-14 22:02:42 -0500

    set up more proof of conecpt home-assistant stuff

--- machines/raptor/home-assistant.nix
+++ machines/raptor/home-assistant.nix
@@ -7,6 +7,11 @@
       "esphome"
       "met"
       "radio_browser"
+      "arest"
+      "rest"
+    ];
+    extraPackages = python3Packages: with python3Packages; [
+      gtts
     ];
     openFirewall = true;
     configWritable = true;
@@ -21,6 +26,26 @@
           max = 10;
           step = 1;
           icon = "mdi:target";
+        };
+      };
+      # Example configuration.yaml entry
+      sensor = {
+        platform = "arest";
+        resource = "http://192.168.1.176:8080/arest";
+        name = "Vesta";
+        monitored_variables = {
+          var_0 = {};
+          var_1 = {};
+          var_2 = {};
+          var_3 = {};
+        };
+      };
+      rest_command = {
+        post_var_0 = {
+          url = "http://192.168.1.176:8080/value_by_name/var_0";
+          method = "PUT";
+          payload = ''{"value": "{{ states('input_number.test_input') | float }}" }'';
+          content_type= "application/json";
         };
       };
     };
