2be2ace4730b06edb6bd1dafd74c9aedef438073
Geometer1729 <16kuhnb@gmail.com>
2024-02-22 14:29:27 -0500

    update option names

--- nix/xmonad.nix
+++ nix/xmonad.nix
@@ -2,11 +2,11 @@
 {
   services.getty.autologinUser = userName;
   services.xserver = {
-    layout = "us";
     enable = true;
     displayManager.startx.enable=true;
     windowManager.xmonad.enable=true;
-    xkbOptions = "caps:swapescape";
+    xkb.options = "caps:swapescape";
+    xkb.layout = "us";
   };
   console.useXkbConfig = true;
   hardware.opengl.enable = true;
