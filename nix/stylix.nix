e385f763f982e7ce408d216e4fae63db122bdcff
Geometer1729 <16kuhnb@gmail.com>
2024-05-26 19:39:18 -0400

    switch theme

--- nix/stylix.nix
+++ nix/stylix.nix
@@ -1,24 +1,25 @@
 {pkgs,...}:
 let
-  isotope =
+  solarized-dark =
 {
   #https://tinted-theming.github.io/base16-gallery/
-  base00 = "000000";
-  base01 = "404040";
-  base02 = "606060";
-  base03 = "808080";
-  base04 = "c0c0c0";
-  base05 = "d0d0d0";
-  base06 = "e0e0e0";
-  base07 = "ffffff";
-  base08 = "ff0000";
-  base09 = "ff9900";
-  base0A = "ff0099";
-  base0B = "33ff00";
-  base0C = "00ffff";
-  base0D = "0066ff";
-  base0E = "cc00ff";
-  base0F = "3300ff";
+  base00 =  "002b36";
+  base01 =  "073642";
+  base02 =  "586e75";
+  base03 =  "657b83";
+  base04 =  "839496";
+  base05 =  "93a1a1";
+  base06 =  "eee8d5";
+  base07 =  "fdf6e3";
+  base08 =  "dc322f";
+  base09 =  "cb4b16";
+  base0A =  "b58900";
+  base0B =  "859900";
+  base0C =  "2aa198";
+  base0D =  "268bd2";
+  base0E =  "6c71c4";
+  base0F =  "d33682";
+
 };
 in
 {
@@ -33,7 +34,7 @@ in
     stylix.image = ./grub/sand.jpg;
     stylix.opacity.terminal = 0.8;
     stylix.polarity = "dark";
-    stylix.base16Scheme = isotope // { base0A = "f7e203"; };
+    stylix.base16Scheme = solarized-dark // { base0F = "982ee6"; };
     stylix.fonts = {
       serif = {
         package = pkgs.nerdfonts;
