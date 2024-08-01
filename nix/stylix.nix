c4de2bdac396ca341fe9deec339dff70c5dd7289
Geometer1729 <16kuhnb@gmail.com>
2024-08-01 17:28:02 -0400

    use override

--- nix/stylix.nix
+++ nix/stylix.nix
@@ -7,45 +7,22 @@
 # xmobar (mostly needs a fix for the part from xmonad)
 # vit (not really, needs a stylix module)
 {pkgs,...}:
-let
-    isotope =
-    {
-      base00 = "000000";
-      base01 = "404040";
-      base02 = "606060";
-      base03 = "808080";
-      base04 = "c0c0c0";
-      base05 = "d0d0d0";
-      base06 = "e0e0e0";
-      base07 = "ffffff";
-      base08 = "ff0000";
-      base09 = "ff9900";
-      base0A = "ff0099";
-      base0B = "33ff00";
-      base0C = "00ffff";
-      base0D = "0066ff";
-      base0E = "cc00ff";
-      base0F = "3300ff";
-    };
-in
 {
   stylix = {
     enable = true;
     image = ./grub/sand.jpg;
     opacity.terminal = 0.9;
-    polarity = "dark";
+    polarity = "dark"; # TODO this doesn't seem to work
     #https://tinted-theming.github.io/base16-gallery/
-    base16Scheme = isotope //
-    {
-      #base08 = ""; #red
-      #base09 = ""; #orange
-      base0A = "#ffff00"; #yellow
-      base0B = "#58FF1E"; # green
-      #base0C = ""; # cyan
-      #base0D = "#ffffff"; # blue
-      base0E = "#bd93f9"; # purple?
-      base0F = "#5f875f"; # brown (cringe) blue
-    };
+    base16Scheme ="${pkgs.base16-schemes}/share/themes/isotope.yaml";
+      #isotope //
+    override =
+      {
+        base0A = "#ffff00"; #yellow
+        base0B = "#58FF1E"; # green
+        base0E = "#bd93f9"; # purple?
+        base0F = "#5f875f"; # brown (cringe) blue
+      };
     fonts = {
       serif = {
         package = pkgs.nerdfonts;
