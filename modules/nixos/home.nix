e0051d570be9206c92698ab57541108d0f9bef16
Geometer1729 <16kuhnb@gmail.com>
2025-01-11 16:29:21 -0500

    fix homeassistant not building

--- modules/nixos/home.nix
+++ modules/nixos/home.nix
@@ -1,4 +1,4 @@
-{flake,pkgs,...}:
+{ pkgs, ... }:
 let
   homeassistant-smartrent = pkgs.fetchFromGitHub {
     owner = "ZacheryThomas";
@@ -6,32 +6,34 @@ let
     rev = "main";
     sha256 = "sha256-FIfTrbIGP7FztvifI9wVLtepQZKwd70xImncmoYGu6k=";
   };
+  smartrent-py =
+    let
+      pname = "smartrent-py";
+      version = "0.4.5";
+    in
+    pkgs.buildPythonPackage {
+      inherit pname version;
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-128T4CtMVTUoQK0V71xk888CP3foBjfYDGeGFWbivdM=";
+      };
+    };
 in
 {
   networking.firewall.allowedTCPPorts = [ 8123 ];
   # TODO try the customComponents field instead
   systemd.tmpfiles.rules = [
-      "C /var/lib/hass/custom_components/smartrent - - - - ${homeassistant-smartrent}/custom_components/smartrent"
-      "Z /var/lib/hass/custom_components 770 hass hass - -"
-    ];
+    "C /var/lib/hass/custom_components/smartrent - - - - ${homeassistant-smartrent}/custom_components/smartrent"
+    "Z /var/lib/hass/custom_components 770 hass hass - -"
+  ];
   services.home-assistant = {
     enable = true;
     extraPackages = python3Packages: with python3Packages; [
       ibeacon-ble
       govee-ble
       websockets
-      pyicloud
-      (let
-        pname = "smartrent-py";
-        version = "0.4.5";
-        in
-        buildPythonPackage {
-        inherit pname version;
-        src = pkgs.fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-128T4CtMVTUoQK0V71xk888CP3foBjfYDGeGFWbivdM=";
-        };
-      })
+      #pyicloud
+      smartrent-py
     ];
     configWritable = true;
     lovelaceConfigWritable = true;
@@ -49,7 +51,7 @@ in
 
     config = {
       # Basic setup configuration
-      default_config = {};
+      default_config = { };
       automation = "!include automations.yaml";
       script = "!include scripts.yaml";
       http = {
