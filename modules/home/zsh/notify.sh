cb53efd11019671c69dbc546953265adc31ef6a1
Geometer1729 <16kuhnb@gmail.com>
2025-01-23 20:30:10 -0500

    improve notify.sh

--- modules/home/zsh/notify.sh
+++ modules/home/zsh/notify.sh
@@ -4,15 +4,21 @@ autoload -Uz add-zsh-hook
 function notify_command_complete() {
   local command=$1
   local start_time=$2
+  local exit=$3
   local end_time=$(date +%s)
   local elapsed=$((end_time - start_time))
+  local session=$(tmux display-message -p "#S")
 
   if [[ $elapsed -gt 10 ]] &&
     { [ $(tmux display-message -p -t $TMUX_PANE '#{?pane_active,1,0}') != 1 ] \
       || [ "$WINDOWID" != "$(xdotool getactivewindow)" ]
     }
   then
-    notify-send "$command finished" "In $elapsed seconds"
+    if [ $exit -eq 0 ]; then
+      notify-send "$command" "Ran on $session taking $elapsed seconds"
+    else
+      notify-send -u critical "$command" "Failed wth $exit on $session after $elapsed seconds"
+    fi
   fi
 }
 
@@ -22,8 +28,9 @@ function preexec() {
 }
 
 function precmd() {
+  exit=$?
   if [[ -n $cmd ]]; then
-    notify_command_complete "$cmd" $cmd_start_time
+    notify_command_complete "$cmd" $cmd_start_time "$exit"
     unset cmd cmd_start_time
   fi
 }
