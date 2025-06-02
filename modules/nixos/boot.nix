2a7c8991000601c3ae1c0bb4bfb7376b206cf8d9
Geometer1729 <16kuhnb@gmail.com>
2025-06-02 19:19:56 -0400

    revert boot improvements

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,21 +1,4 @@
-{ config, lib, ... }:
-let
-  wait = {
-    after = [ "network-online.target" "dhcpcd.service" ];
-    wants = [ "network-online.target" "dhcpcd.service" ];
-  };
-  waitAndRestart = wait // {
-    serviceConfig = {
-      # Force cloudflared to wait until DHCP and DNS are fully operational
-      ExecStartPre = "/run/current-system/sw/bin/sleep 5"; # Optional buffer
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
-  };
-  dontWaitFor = {
-    wantedBy = lib.mkForce [ ];
-  };
-in
+{ ... }:
 {
   boot = {
     #kernel.sysctl = {
@@ -35,23 +18,53 @@ in
 
 
   # Boot faster
-  networking = {
-    dhcpcd.enable = true;
-    dhcpcd.wait = "background";
-    dhcpcd.extraConfig = ''
-      timeout 1
-      noarp
-      nodelay
-    '';
-  };
-  systemd.targets = {
-    home-assistant = dontWaitFor;
-  };
-  systemd.services = {
-    ntpd = waitAndRestart;
-    sshd = wait;
-    home-assistant = waitAndRestart;
-    "cloudflared-tunnel-${config.cloudflare-id}" = waitAndRestart // dontWaitFor;
-    cloudflare-warp = dontWaitFor;
-  };
+
+  # keeps breaking cloudflare I give up
+  #let
+  #  wait = {
+  #    after = [ "network-online.target" "dhcpcd.service" ];
+  #    wants = [ "network-online.target" "dhcpcd.service" ];
+  #  };
+  #  waitAndRestart = wait // {
+  #    serviceConfig = {
+  #      # Force cloudflared to wait until DHCP and DNS are fully operational
+  #      ExecStartPre = "/run/current-system/sw/bin/sleep 5"; # Optional buffer
+  #      Restart = "on-failure";
+  #      RestartSec = "5s";
+  #    };
+  #  };
+  #  dontWaitFor = {
+  #    wantedBy = lib.mkForce [ ];
+  #  };
+  #in
+  #networking = {
+  #  dhcpcd.enable = true;
+  #  dhcpcd.wait = "background";
+  #  dhcpcd.extraConfig = ''
+  #    timeout 1
+  #    noarp
+  #    nodelay
+  #  '';
+  #};
+
+  #systemd.network.networks = {
+  # internet0 = {
+  #   matchConfig = { Name = "enp4s0"; };
+  #   networkConfig = { DHCP = "ipv4"; };
+  #  };
+  #};
+
+  #systemd = {
+  #  targets = {
+  #    home-assistant = dontWaitFor;
+  #  };
+  #  services = {
+  #    ntpd = waitAndRestart;
+  #    sshd = wait;
+  #    home-assistant = waitAndRestart;
+  #    "cloudflared-tunnel-${config.cloudflare-id}" = waitAndRestart // dontWaitFor;
+  #    cloudflare-warp = dontWaitFor;
+  #  };
+  #};
+
 }
