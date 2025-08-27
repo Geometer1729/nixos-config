0b507c2dae67d366113621dd547ac429df0a690f
Geometer1729 <16kuhnb@gmail.com>
2025-08-27 09:35:09 -0400

    F

--- modules/nixos/work.nix
+++ modules/nixos/work.nix
@@ -37,42 +37,42 @@ let
 
 in
 {
-  services.postgresql = {
-    enable = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  ADDRESS METHOD
-      local all all trust
-      host  all all 0.0.0.0/0 trust
-      host  all all ::0/0 trust
-    '';
-    # this probably doesn't help either tbh
-    settings = {
-      listen_addresses = pkgs.lib.mkOverride 10 "*";
-    };
-    # doesn't work
-    #initialScript = pkgs.writeText "init-sql-script"
-    #''
-    #  alter user postgres with password 'postgres';
-    #'';
-  };
-  #users.users.postgres.password = "postgres";
+  #services.postgresql = {
+  #  enable = true;
+  #  authentication = pkgs.lib.mkOverride 10 ''
+  #    #type database  DBuser  ADDRESS METHOD
+  #    local all all trust
+  #    host  all all 0.0.0.0/0 trust
+  #    host  all all ::0/0 trust
+  #  '';
+  #  # this probably doesn't help either tbh
+  #  settings = {
+  #    listen_addresses = pkgs.lib.mkOverride 10 "*";
+  #  };
+  #  # doesn't work
+  #  #initialScript = pkgs.writeText "init-sql-script"
+  #  #''
+  #  #  alter user postgres with password 'postgres';
+  #  #'';
+  #};
+  ##users.users.postgres.password = "postgres";
 
-  #users.users.postgres.password = "";
+  ##users.users.postgres.password = "";
 
-  #virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "bbrian" ];
-  #virtualisation.vmware.host.enable = true;
-  #virtualisation.vmware.guest.enable = true;
-  virtualisation.docker.enable = true;
-  users.users.bbrian.extraGroups = [ "docker" ];
+  ##virtualisation.virtualbox.host.enable = true;
+  #users.extraGroups.vboxusers.members = [ "bbrian" ];
+  ##virtualisation.vmware.host.enable = true;
+  ##virtualisation.vmware.guest.enable = true;
+  #virtualisation.docker.enable = true;
+  #users.users.bbrian.extraGroups = [ "docker" ];
   users.users.bbrian.packages = [ clorio ];
-  nix.settings = {
-    extra-substituters = [ "https://storage.googleapis.com/mina-nix-cache" ];
-    extra-trusted-public-keys = [
-      "nix-cache.minaprotocol.org:fdcuDzmnM0Kbf7yU4yywBuUEJWClySc1WIF6t6Mm8h4="
-      "nix-cache.minaprotocol.org:D3B1W+V7ND1Fmfii8EhbAbF1JXoe2Ct4N34OKChwk2c="
-      "mina-nix-cache-1:djtioLfv2oxuK2lqPUgmZbf8bY8sK/BnYZCU2iU5Q10="
-    ];
-  };
+  #nix.settings = {
+  #  extra-substituters = [ "https://storage.googleapis.com/mina-nix-cache" ];
+  #  extra-trusted-public-keys = [
+  #    "nix-cache.minaprotocol.org:fdcuDzmnM0Kbf7yU4yywBuUEJWClySc1WIF6t6Mm8h4="
+  #    "nix-cache.minaprotocol.org:D3B1W+V7ND1Fmfii8EhbAbF1JXoe2Ct4N34OKChwk2c="
+  #    "mina-nix-cache-1:djtioLfv2oxuK2lqPUgmZbf8bY8sK/BnYZCU2iU5Q10="
+  #  ];
+  #};
 
 }
