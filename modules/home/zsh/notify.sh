8eaaf2a0961cd4fd236729003c77d7d8bf2d2f58
Geometer1729 <16kuhnb@gmail.com>
2025-10-03 11:47:09 -0400

    fix notify I think?

--- modules/home/zsh/notify.sh
+++ modules/home/zsh/notify.sh
@@ -1,6 +1,26 @@
 #Notify me when a command finishes in an unfocused pane after more than 10 seconds
 autoload -Uz add-zsh-hook
 
+function is_window_focused() {
+  if [[ -n "$WAYLAND_DISPLAY" ]]; then
+    # On Wayland with Hyprland, use tmux to get terminal PID and check if it's focused
+    if [[ -n "$TMUX" ]]; then
+      local terminal_pid=$(tmux display-message -p '#{client_pid}')
+      local focused_pid=$(pgrep -P $(hyprctl activewindow -j | jq -r '.pid'))
+      pgrep -P $focused_pid | grep $terminal_pid > /dev/null
+    else
+      # Not in tmux don't notify
+      return 0
+    fi
+  elif [[ -n "$DISPLAY" && -n "$WINDOWID" ]]; then
+    # On X11, use xdotool to check if current window is active
+    [[ "$WINDOWID" == "$(xdotool getactivewindow 2>/dev/null)" ]]
+  else
+    # Fallback don't notify?
+    return 0
+  fi
+}
+
 function notify_command_complete() {
   local command=$1
   local start_time=$2
@@ -11,7 +31,7 @@ function notify_command_complete() {
 
   if [[ $elapsed -gt 10 ]] &&
     { [ $(tmux display-message -p -t $TMUX_PANE '#{?pane_active,1,0}') != 1 ] \
-      || [ "$WINDOWID" != "$(xdotool getactivewindow)" ]
+      || ! is_window_focused
     }
   then
     if [ $exit -eq 0 ]; then
