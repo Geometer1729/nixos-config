849d9a140cddf0c2aef926dae8943effe864bab4
Geometer1729 <16kuhnb@gmail.com>
2025-10-07 16:48:41 -0400

    now a bit too muted

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -24,51 +24,60 @@
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/deep-oceanic-next.yaml";
     base16Scheme =
       let
-          black       = "#000000";
-          darkred     = "#800000";
-          darkgreen   = "#008000";
-          darkyellow  = "#808000";
-          darkblue    = "#000080";
-          darkmagenta = "#800080";
-          darkcyan    = "#008080";
-          gray        = "#c0c0c0";
-          darkgray    = "#808080";
-          red         = "#a8361b";
-          green       = "#00ff00";
-          yellow      = "#ffff00";
-          blue        = "#0000ff";
-          magenta     = "#ff00ff";
-          cyan        = "#00ffff";
-          white       = "#d7d4cd";
-          jbrown      = "#FA3B17";
-          jred        = "#ff0000";
-          jgray       = "#4e4e4e";
-          jgreen      = "#58FF1E";
-          jdarkgreen  = "#5f875f";
-          jpurple     = "#bd93f9";
-          jblack      = "#121212";
-          jorange     = "#FF8B00";
-          jyellow     = "#ffff00";
+        black = "#000000";
+        darkred = "#800000";
+        darkgreen = "#008000";
+        darkyellow = "#808000";
+        darkblue = "#000080";
+        darkmagenta = "#800080";
+        darkcyan = "#008080";
+        gray = "#c0c0c0";
+        darkgray = "#808080";
+        red = "#a8361b";
+        green = "#00ff00";
+        yellow = "#ffff00";
+        blue = "#0000ff";
+        magenta = "#ff00ff";
+        cyan = "#00ffff";
+        white = "#d7d4cd";
+        jbrown = "#FA3B17";
+        jred = "#ff0000";
+        jgray = "#4e4e4e";
+        jgreen = "#58FF1E";
+        jdarkgreen = "#5f875f";
+        jpurple = "#bd93f9";
+        jblack = "#121212";
+        jorange = "#FF8B00";
+        jyellow = "#ffff00";
+
+        # Softer alternatives to reduce eye strain
+        soft_green = "#87af87"; # Much easier on eyes than #00ff00
+        soft_yellow = "#d7d75f"; # Toned down yellow
+        soft_blue = "#5f87d7"; # Softer than pure blue
+        soft_cyan = "#5fafaf"; # Muted cyan
+        soft_purple = "#af87d7"; # Gentler purple
+        soft_red = "#d75f5f"; # Softer red
+        darker_bg = "#1a1a1a"; # Slightly lighter background
       in
       {
-          name = "joker";
+        name = "joker";
 
-          base00 = black;
-          base01 = darkred;
-          base02 = darkgreen;
-          base03 = jorange;
-          base04 = darkblue;
-          base05 = jpurple;
-          base06 = cyan;
-          base07 = gray;
-          base08 = darkgray;
-          base09 = red;
-          base0A = jgreen;
-          base0B = jyellow;
-          base0C = blue;
-          base0D = magenta; # TODO repeated
-          base0E = cyan;
-          base0F = white;
+        base00 = jblack; # Background - softer than pure black
+        base01 = jgray; # Lighter background
+        base02 = darkgray; # Selection background
+        base03 = darkred; # Comments
+        base04 = gray; # Dark foreground
+        base05 = white; # Default foreground
+        base06 = gray; # Light foreground
+        base07 = white; # Light background
+        base08 = soft_red; # Red - using softer version
+        base09 = jorange; # Orange
+        base0A = soft_yellow; # Yellow - much easier on eyes
+        base0B = soft_green; # Green - way less harsh
+        base0C = soft_cyan; # Cyan - softer
+        base0D = soft_blue; # Blue - gentler
+        base0E = jpurple; # Magenta/Purple - already nice
+        base0F = jdarkgreen; # Extra color
       };
     #"${pkgs.base16-schemes}/share/themes/isotope.yaml";
     ##isotope //
