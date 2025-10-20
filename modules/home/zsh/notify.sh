6aa1d876a07d0c3cc0a9a045b13f15e2746f1084
Geometer1729 <16kuhnb@gmail.com>
2025-10-20 15:17:07 -0400

    silence all the errors from notify.sh when it fails due to ssh being root etc

--- modules/home/zsh/notify.sh
+++ modules/home/zsh/notify.sh
@@ -5,9 +5,9 @@ function is_window_focused() {
   if [[ -n "$WAYLAND_DISPLAY" ]]; then
     # On Wayland with Hyprland, use tmux to get terminal PID and check if it's focused
     if [[ -n "$TMUX" ]]; then
-      local terminal_pid=$(tmux display-message -p '#{client_pid}')
-      local focused_pid=$(hyprctl activewindow -j | jq -r '.pid')
-      pgrep -P $focused_pid | grep $terminal_pid > /dev/null
+      local terminal_pid=$(tmux display-message -p '#{client_pid}' 2>/dev/null)
+      local focused_pid=$(hyprctl activewindow -j 2>/dev/null | jq -r '.pid' 2>/dev/null)
+      [[ -n "$terminal_pid" && -n "$focused_pid" ]] && pgrep -P $focused_pid 2>/dev/null | grep $terminal_pid > /dev/null 2>&1
     else
       # Not in tmux don't notify
       return 0
@@ -27,17 +27,22 @@ function notify_command_complete() {
   local exit=$3
   local end_time=$(date +%s)
   local elapsed=$((end_time - start_time))
-  local session=$(tmux display-message -p "#S")
+  local session=$(tmux display-message -p "#S" 2>/dev/null)
+
+  # Skip notifications if we can't access tmux or notify-send
+  if ! command -v notify-send &>/dev/null || [[ -z "$session" ]]; then
+    return 0
+  fi
 
   if [[ $elapsed -gt 10 ]] &&
-    { [ $(tmux display-message -p -t $TMUX_PANE '#{?pane_active,1,0}') != 1 ] \
+    { [ $(tmux display-message -p -t $TMUX_PANE '#{?pane_active,1,0}' 2>/dev/null) != 1 ] \
       || ! is_window_focused
     }
   then
     if [ $exit -eq 0 ]; then
-      notify-send "$command" "Ran on $session taking $elapsed seconds"
+      notify-send "$command" "Ran on $session taking $elapsed seconds" 2>/dev/null
     else
-      notify-send -u critical "$command" "Failed wth $exit on $session after $elapsed seconds"
+      notify-send -u critical "$command" "Failed wth $exit on $session after $elapsed seconds" 2>/dev/null
     fi
   fi
 }
