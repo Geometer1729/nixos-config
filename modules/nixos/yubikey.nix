67559ab98935b780cc8fd51b4255f64d3200c29a
Geometer1729 <16kuhnb@gmail.com>
2025-10-23 15:28:11 -0400

    Try to fix yubikey lock issue

--- modules/nixos/yubikey.nix
+++ modules/nixos/yubikey.nix
@@ -27,7 +27,10 @@
 
   # Add udev rules for YubiKey
   services.udev.extraRules = ''
-    # YubiKey
+    # YubiKey permissions
     SUBSYSTEM=="usb", ATTR{idVendor}=="1050", ATTR{idProduct}=="0407", MODE="0660", GROUP="users"
+
+    # Kill scdaemon when YubiKey is removed to prevent exclusive lock issues
+    ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", ENV{ID_MODEL_ID}=="0407", RUN+="${pkgs.gnupg}/bin/gpgconf --kill scdaemon"
   '';
 }
