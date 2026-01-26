f5bf772f0f125dc6a4aa500f5647306b55373c06
Geometer1729 <16kuhnb@gmail.com>
2026-01-26 08:38:25 -0500

    Fix prism overlay
    Problem: Some mod uses vlc
    
    Solution: Provide it in the override

--- overlays/prismlauncher.nix
+++ overlays/prismlauncher.nix
@@ -1,11 +1,10 @@
 { ... }:
 final: prev: {
-  # Some mods require nss this overlay fixes it
-  prismlauncher = prev.prismlauncher.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [ final.nss ];
-    postInstall = (oldAttrs.postInstall or "") + ''
-      wrapProgram $out/bin/prismlauncher \
-        --prefix LD_LIBRARY_PATH : "${final.lib.makeLibraryPath [ final.nss ]}"
-    '';
-  });
+  # Some mods require nss and vlc (for watermedia)
+  prismlauncher = prev.prismlauncher.override {
+    additionalLibs = [
+      final.libvlc # Required for watermedia mod
+      final.nss # Required for some mods
+    ];
+  };
 }
