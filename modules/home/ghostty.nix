4dde5415b5275a10988dd4ecf6f01d6b7a0d131d
Geometer1729 <16kuhnb@gmail.com>
2026-03-09 09:26:03 -0400

    fix ghostty tmux

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -5,7 +5,7 @@
     settings = {
       window-padding-x = 3;
       window-padding-y = 3;
-      initial-command = "${pkgs.tmux}/bin/tmux";
+      command = "${pkgs.tmux}/bin/tmux";
       window-theme = "ghostty";
       gtk-titlebar = false;
       confirm-close-surface = false;
