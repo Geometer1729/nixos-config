3a8c7fc5655e69a6b1ebb0c4d42a2f03e6eab696
Geometer1729 <16kuhnb@gmail.com>
2026-09-03 19:06:50 -0400

    turn on other nice features

--- modules/home/nvim/lua/render-markdown.lua
+++ modules/home/nvim/lua/render-markdown.lua
@@ -1,17 +1,5 @@
 require('render-markdown').setup({
   file_types = { 'markdown', 'vimwiki' },
-  heading = { enabled = false },
-  code = { enabled = false },
-  dash = { enabled = false },
-  bullet = { enabled = false },
-  checkbox = { enabled = false },
-  quote = { enabled = false },
-  link = { enabled = false },
-  sign = { enabled = false },
-  latex = { enabled = false },
-  inline_highlight = { enabled = false },
-  html = { enabled = false },
-  yaml = { enabled = false },
   pipe_table = {
     preset = 'round',
     cell = 'padded',
