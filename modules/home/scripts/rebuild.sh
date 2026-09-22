10a8f43cedb4d6fdcfb83044979fe5cca71d0b9b
Geometer1729 <16kuhnb@gmail.com>
2026-09-22 12:36:45 -0400

    auto pull on rebuild

--- modules/home/scripts/rebuild.sh
+++ modules/home/scripts/rebuild.sh
@@ -1,3 +1,14 @@
+if git -C ~/conf diff --quiet && git -C ~/conf diff --cached --quiet
+then
+  if ! git -C ~/conf pull --ff-only
+  then
+    zsh
+    exit 1
+  fi
+else
+  echo "Skipping git pull: ~/conf has staged or unstaged changes."
+fi
+
 if unbuffer nh os test ~/conf 2>&1 | tee ~/Downloads/nixerr
 then
   # Hide scratchpad after successful rebuild if enabled
