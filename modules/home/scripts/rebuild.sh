9cf6827984c52fd16f2860cad9d50d5ab0392d25
Geometer1729 <16kuhnb@gmail.com>
2026-09-22 12:44:31 -0400

    tweak rebuild script again

--- modules/home/scripts/rebuild.sh
+++ modules/home/scripts/rebuild.sh
@@ -9,7 +9,7 @@ else
   echo "Skipping git pull: ~/conf has staged or unstaged changes."
 fi
 
-if unbuffer nh os test ~/conf 2>&1 | tee ~/Downloads/nixerr
+if unbuffer nh os switch ~/conf 2>&1 | tee ~/rebuild-log
 then
   # Hide scratchpad after successful rebuild if enabled
   "$HIDE_AFTER_REBUILD" && scratchPad sp hide
